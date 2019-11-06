1488eeaf27aba32bd8eb7f3bb177e7a4c3953fb0
unknown <upornij@gmail.com>
2019-11-07 00:19:53 +0200

    commit 2..5

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,15 @@
  * @returns {Object}
  */
 function clone(obj) {
+  if (!obj) return obj;
+
+  let res = {};
+  for (let key in obj) {
+    let value = obj[key];
+    if (typeof (value) == 'object') {
+      value = clone(value);
+    }
+    res[key] = value;
+  }
+  return res;
 }
