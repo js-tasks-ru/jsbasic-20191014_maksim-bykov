7365633e7cc66586c93239a4f873945b69d9299e
jwriter <upornij@gmail.com>
2019-12-05 15:39:15 +0200

    7 и  8

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -11,6 +11,56 @@ class TimeLeft {
    */
   constructor(el) {
     this.el = el;
+    let from = this.parseDate(el.dataset['from']);
+    let to = this.parseDate(el.dataset['to']);
+
+    if (!from || !to || from > to) {
+      el.innerHTML = 'завершенно';
+      return;
+    }
+
+    let msg = '';
+    let dif = to - from;
+    let days = Math.trunc(dif / MS_IN_DAY); 
+    dif = dif - days * MS_IN_DAY;
+    msg += this.makeString(days, ['день', 'дня', 'дней']);
+
+    let hours = Math.trunc(dif / MS_IN_HOUR); 
+    dif = dif - hours * MS_IN_HOUR;
+    msg += this.makeString(hours, ['час', 'часа', 'часов']);
+    
+    let mins = Math.trunc(dif / MS_IN_MINUTE); 
+    dif = dif - mins * MS_IN_MINUTE;
+    msg += this.makeString(mins, ['минута', 'минуты', 'минут']);
+    
+    let secs = Math.trunc(dif / MS_IN_SEC); 
+    dif = dif - secs * MS_IN_SEC;
+    msg += this.makeString(secs, ['секунда', 'секунды', 'секунд']);
+
+    if (msg.startsWith(',')) {
+      msg = msg.substring(1);
+      //msg.trimLeft();
+    }
+    el.innerHTML = msg.trim();
+  }
+
+  makeString(value, arr) {
+    if (value == 0) {
+      return '';
+    }
+    let str = '' + value;
+
+    if (value > 5 && value < 20) {
+      return ', ' + value + ' ' + arr[2];
+    }
+    if (str.endsWith('1')) {
+      return ', ' + value + ' ' + arr[0];
+    }
+    if (str.endsWith('2') || str.endsWith('3') || str.endsWith('4')) {
+      return ', ' + value + ' ' + arr[1];
+    }
+    return ', ' + value + ' ' + arr[2];
+    
   }
 
   /**
@@ -18,7 +68,9 @@ class TimeLeft {
    * @param {string} str - строка с датой в формате `year.month.day hours:minutes:second`
    * @return {Date} - возвращаем объект даты
    */
-  parseDate(str) {}
+  parseDate(str) {
+    return Date.parse(str);
+  }
 
   /**
    * Статчный метод, который можно вызывать не посредственно от класса, а не от объекта.
--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,13 +1,106 @@
+//import { promises } from 'dns';
+
 class ProductList {
   productsUrl = '/assets/data/products.json';
   productsStoreKey = 'cart-products';
 
   constructor(element) {
-    this.el = element;
+    this.el = element;    
   }
 
-  show() {
+  async show() {
+    await fetch(this.productsUrl)
+      .then(res => res.json())
+      .then((data) => {
+        this.data = data;
+        let content = this.drawList(data);
+        this.el.innerHTML = content;
+
+        let cartlist = this.el.querySelector('.homepage-cards');
+        cartlist.addEventListener('click', this.addHandler);
+      });
   }
+
+  addHandler(event) {
+    let trg = event.target;
+    if (trg.className == 'product-add-to-cart') {
+      let parent = trg.closest('.products-list-product');
+      let doIt = confirm('Вы уверенны, что хотите добавить этот товар в корзину?');
+      if (doIt) {
+        let productId = +parent.dataset['productId'];
+        let cart = localStorage.getItem('cart-products');
+        let cartData = cart ? JSON.parse(cart) : [];
+        if (!cartData.some(cart => cart.id == productId)) {
+          cartData.push({id: productId});
+        }
+        localStorage.setItem('cart-products', JSON.stringify(cartData));
+      }
+    }
+  }
+
+  drawList(productData) {
+    let productList = '';
+    productData.forEach(element => {
+      productList += this.drawCard(element);
+    });
+
+    return `
+    <div class="row justify-content-end">
+      <div class="col-lg-9">
+        <h3 class="section-title">Top Recommendations for You</h3>
+        <div class="row homepage-cards">    
+          ${productList}        
+        </div>
+      </div>
+    </div>`;
+  }
+
+  drawCard(product) {
+    let stars = '';
+    for (let i = 1; i <= 5; i++) {
+      if (product.rating) {
+        if (i <= product.rating.stars) {
+          stars += '<i class="icon-star checked"></i>';
+        } else {
+          stars += '<i class="icon-star active"></i>';
+        }
+      } else {
+        stars += '<i class="icon-star"></i>';
+      }
+    }
+
+    let rating = `
+      <div class="rate">
+        ${stars}
+        <span class="rate-amount ml-2">${product.rating ? product.rating.reviewsAmount : ''}</span>
+      </div>    
+    `;
+        
+    let price = `<p class="card-text price-text discount"><strong>${product.price}</strong>`;
+    if (product.oldPrice) {
+      price += `<small class="ml-2">${product.oldPrice}</small></p>`;
+    }
+
+    let card = `
+    <div data-product-id="${product.id}" class="products-list-product col-md-6 col-lg-4 mb-4">
+      <div class="card">
+        <div class="card-img-wrap">
+          <img class="card-img-top" src="${product.imageUrl}" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${product.title}</h5>
+          ${rating}
+          ${price}          
+          <button class="product-add-to-cart" data-button-role="add-to-cart">
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+    `;
+    return card;
+  }
+
 }
 
 // Делает класс доступным глобально, сделано для упрощения, чтобы можно было его вызывать из другого скрипта
